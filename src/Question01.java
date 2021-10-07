044206e5a9d94e715416c48085d793c5e9755423
Chimoha31 <mokochii1108@gmail.com>
2021-10-07 14:36:49 -0700

    fixed variable name for final

--- src/Question01.java
+++ src/Question01.java
@@ -2,14 +2,16 @@
 class Question01 {
 
 	public static void main(String[] args) {
+//	    String
 		String name = "Chiho Maekawa";
 		System.out.println(name);
+		
 //		変数
 		int num1 = 12;
 		System.out.println(num1);
 		
 //		定数
-		final char letter = 'a';
-		System.out.println(letter);
+		final char FIRST_LETTER = 'a';
+		System.out.println(FIRST_LETTER);
 	}
 }
